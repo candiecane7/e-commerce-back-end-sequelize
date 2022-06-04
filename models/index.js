bf21857b3a3cd9c2b1b7faeb2ff3102bd54cf465
Candice Hall <hall.candice@outlook.com>
2022-06-04 16:56:28 -0400

    wrote out models

---
+++ models/index.js
@@ -0,0 +1,28 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 
+}
+  )
+
+// Tags belongToMany Products (through ProductTag)
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
