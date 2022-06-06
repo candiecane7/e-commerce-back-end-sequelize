10d8ffd4e3e7f2a109705bbb8fb88eba18afeab4
Candice Hall <hall.candice@outlook.com>
2022-06-05 20:25:22 -0400

    added category routes and testing in insomnia. also set up sequelize in server.js and ran seeds

--- models/index.js
+++ models/index.js
@@ -10,15 +10,22 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 
+  foreignKey: 'product_id'
 }
   )
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
